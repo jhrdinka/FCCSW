07163bc8550e25f415f6ec11120e7476f6ac75f7
Valentin Volkl <valentin.volkl@cern.ch>
2016-07-06 20:50:05 +0200

    Test the tracker.

--- Examples/options/geant_fullsim_pgun_geantino.py
+++ Examples/options/geant_fullsim_pgun_geantino.py
@@ -7,17 +7,17 @@ podioevent = FCCDataSvc("EventDataSvc")
 # DD4hep geometry service
 # Parses the given xml file
 from Configurables import GeoSvc, SimG4SingleParticleGeneratorTool
-geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhTrackerSimple/compact/FCChh_TrackerStandalone.xml'],
-                    OutputLevel = DEBUG)
+geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml', 'file:Detector/DetFCChhTrackerSimple/compact/FCChh_GenericTracker.xml'],
+    OutputLevel = DEBUG)
 
 
 # Geant4 service
 # Configures the Geant simulation: geometry, physics list and user actions
 from Configurables import SimG4Svc
 # giving the names of tools will initialize the tools of that type
-geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4FtfpBert", actions="SimG4FullSimActions")
+geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4GeantinoDeposits", actions="SimG4FullSimActions")
 
-geantservice.G4commands += ["/tracking/verbose 1"]
+#geantservice.G4commands += ["/tracking/verbose 1"]
 
 # Geant4 algorithm
 # Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
@@ -30,7 +30,7 @@ savetrackertool.DataOutputs.trackClusters.Path = "clusters"
 savetrackertool.DataOutputs.trackHits.Path = "hits"
 savetrackertool.DataOutputs.trackHitsClusters.Path = "hitClusterAssociation"
 # next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
-pgun = SimG4SingleParticleGeneratorTool("GeantinoGun", etaMin=0, etaMax=0.01, particleName="geantino")
+pgun = SimG4SingleParticleGeneratorTool("GeantinoGun", etaMin=-0.2, etaMax=0.2, phiMin=0, phiMax=2*3.14, particleName="geantino")
 geantsim = SimG4Alg("SimG4Alg",
                     outputs= ["SimG4SaveTrackerHits/saveTrackerHits" ],
                     eventProvider=pgun)
@@ -47,7 +47,7 @@ out.outputCommands = ["keep *"]
 from Configurables import ApplicationMgr
 ApplicationMgr( TopAlg = [geantsim, out],
                 EvtSel = 'NONE',
-                EvtMax   = 10,
+                EvtMax   = 10000,
                 # order is important, as GeoSvc is needed by SimG4Svc
                 ExtSvc = [podioevent, geoservice, geantservice],
                 OutputLevel=DEBUG
